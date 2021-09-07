e9f4aed9abff6c554b3e0e229b6f2cb4e2a47528
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2021-09-06 22:39:11 -0300

    agregado CRUD de continentes

--- app/controllers/api/v1/continentes_controller.rb
+++ app/controllers/api/v1/continentes_controller.rb
@@ -2,19 +2,45 @@ module Api
   module V1
     class ContinentesController < ApplicationController
       def index
-        continentes = Continente.find();
+        continentes = Continente.all
+        render json: continentes, status: :ok
       end
     
       def show
+        continentes = Continente.find(params[:id])
+        render json: continentes, status: :ok
       end
     
       def create
+        continente = Continente.new(parametrosContinente)
+        if continente.save
+          render json: continente, status: :ok
+        else
+          render json: continente.errors, status: :unprocessable_entity
+        end
       end
     
       def update
+        continente = Continente.find(params[:id])
+        if continente.update_attributes(parametrosContinente)
+          render json: Continente, status: :ok
+        else
+          render json: continente.errors, status: :unprocessable_entity
+        end
       end
     
-      def delete
+      def destroy
+        continente = Continente.find(params[:id])
+        if continente.destroy
+          render json: continente, status: :ok
+        else
+          render json: continente.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def parametrosContinente
+        params.permit(:denominacion, :imagen)
       end
     end
   end
