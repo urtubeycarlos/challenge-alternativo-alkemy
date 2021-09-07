fbf585e54690ae7732b448e4143ad2d4e430805c
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2021-09-07 19:41:53 -0300

    mejor codigo de controlador de contienentes

--- app/controllers/api/v1/continentes_controller.rb
+++ app/controllers/api/v1/continentes_controller.rb
@@ -1,46 +1,50 @@
 module Api
   module V1
     class ContinentesController < ApplicationController
+      before_action :set_continente, only: [:show, :update, :destroy]
+      
       def index
-        continentes = Continente.all
-        render json: continentes, status: :ok
+        @continentes = Continente.all
+        render json: @continentes, status: :ok
       end
     
       def show
-        continentes = Continente.find(params[:id])
-        render json: continentes, status: :ok
+        render json: @continente, status: :ok
       end
     
       def create
-        continente = Continente.new(parametrosContinente)
-        if continente.save
-          render json: continente, status: :ok
+        puts "Parametros: #{params}"
+        @continente = Continente.new(parametros_continente)
+        if @continente.save
+          render json: @continente, status: :ok
         else
-          render json: continente.errors, status: :unprocessable_entity
+          render json: @continente.errors, status: :unprocessable_entity
         end
       end
     
       def update
-        continente = Continente.find(params[:id])
-        if continente.update_attributes(parametrosContinente)
-          render json: Continente, status: :ok
+        if @continente.update(parametros_continente)
+          render json: @continente, status: :ok
         else
-          render json: continente.errors, status: :unprocessable_entity
+          render json: @continente.errors, status: :unprocessable_entity
         end
       end
     
       def destroy
-        continente = Continente.find(params[:id])
-        if continente.destroy
-          render json: continente, status: :ok
+        if @continente.destroy
+          render json: @continente, status: :ok
         else
-          render json: continente.errors, status: :unprocessable_entity
+          render json: @continente.errors, status: :unprocessable_entity
         end
       end
 
       private
-      def parametrosContinente
-        params.permit(:denominacion, :imagen)
+      def parametros_continente
+        params.require(:continente).permit(:denominacion, :imagen)
+      end
+
+      def set_continente
+        @continente = Continente.find(params[:id])
       end
     end
   end
