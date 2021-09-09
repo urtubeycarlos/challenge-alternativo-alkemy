a0613b92dccae7f4db04501e5f6fd0c673922bca
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2021-09-09 15:00:18 -0300

    agregada rutas paises

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'paises/index'
-      get 'paises/show'
-      get 'paises/create'
-      get 'paises/update'
-      get 'paises/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'continentes/index'
-      get 'continentes/show'
-      get 'continentes/create'
-      get 'continentes/update'
-      get 'continentes/delete'
-    end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :continentes
+      resources :paises
     end
   end
 end
